ebe0251495e2551ad98e8b3fc565b96f934a7f26
xuxian94 <xuxian@xuxiandeMacBook-Pro.local>
2017-03-02 23:58:28 +0800

    python

--- easyblog/urls.py
+++ easyblog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index , name='index'),
     url(r'^article/(?P<blog_body_id>[0-9]+)/$',views.article,name='article'),
+    url(r'^python/',views.artical_python,name='python')
 ]
--- easyblog/views.py
+++ easyblog/views.py
@@ -13,3 +13,9 @@ def article(request , blog_body_id='0'):
     print(blog_body_id)
     blog_content = BlogBody.objects.get(id=blog_body_id)
     return render(request,'view.html',{'blog_content':blog_content})
+
+def artical_python(request):
+    # sql = 'select id, blog_title, blog_type, blog_timestamp, blog_body from grzx_blogbody WHERE blog_type = "Python"'
+    # artical_python_blog = BlogBody.objects.raw(sql)
+    artical_python_blog = BlogBody.objects.filter(blog_type='python')
+    return render(request,'artical_python_list.html',{'artical_python_blog':artical_python_blog})
